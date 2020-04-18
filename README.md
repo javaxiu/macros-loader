70bd120dbce5465bb752e592923deaad2d71f853
javaer.ljw <javaer.ljw@alibaba-inc.com>
2020-04-18 20:39:42 +0800

    init

---
+++ README.md
@@ -0,0 +1,31 @@
+# Preprocessor for Webpack
+
+fork from git+https://github.com/jdmichaud/preprocessor-loader.git
+
+# why
+'cause author stop maintaining this project, and [my issue](https://github.com/jdmichaud/preprocessor-loader/issues/1) was left to open for a while
+
+# use
+
+// webpack.config.js
+
+{
+  module: {
+    loaders: [
+      { test: /\.js$/,
+        loader: 'pprocessor?config=pprocessor-loader.json',
+        exclude: [/bower_components/, /node_modules/],
+      },
+    ],
+  }
+}
+
+// pprocessor-loader.json
+
+"macros": [
+    {
+      "declaration" : "LOG_INFO(message)",
+      "definition" : "console.log(message + ' (__FILE__:__LINE__)');",
+      "REG": "/(src:?)(.*)/"
+    },
+]
---
+++ index.js
@@ -0,0 +1,97 @@
+/* eslint prefer-template: 0 */
+/* eslint no-useless-escape: 0 */
+
+/*
+  MIT License http://www.opensource.org/licenses/mit-license.php
+  Author jean.daniel.michaud@gmail.com
+*/
+var fs = require('fs');
+var path = require('path');
+var loaderUtils = require('loader-utils');
+
+function replaceFilename(content, filename, reg) {
+  filename = filename.replace(/\\/g, '/');
+  return content.replace(/__FILE__/g, (filename.match(reg) || [filename])[0]);
+}
+
+function replaceLinenumber(content) {
+  return content
+          .split('\n')
+          .map((line, index) => line.replace(/__LINE__/g, index + 1))
+          .join('\n');
+}
+
+function preprocessMacros(macros) {
+  macros.forEach((macro) => {
+    const split = macro.declaration.split(/[(),]/).filter(Boolean);
+    macro.name = split[0].trim();
+    macro.args = split.slice(1).map(arg => arg.trim());
+  });
+  return macros;
+}
+
+function buildSubstitution(expression, macro) {
+  const split = expression.split(/[(),]/).filter(Boolean);
+  let result = macro.definition;
+  macro.args.forEach((arg, index) => {
+    result = result.replace(new RegExp(arg, 'g'), split[index + 1]);
+  });
+  return result;
+}
+
+function replaceMacros(content, macros) {
+  var result = content;
+  macros.forEach((macro) => {
+    if (macro.args.length === 0) {
+      // Macro has no arguments, easy
+      result = result.replace(new RegExp(macro.name + '\s*(\\(\\))?', 'g'),
+                              macro.definition);
+    } else {
+      // Macro has arguments
+      // First retrieve the expressions to replace
+      const expressions = result.match(new RegExp(macro.name + '\s*\\([^\)]+\\)', 'g'));
+      if (expressions) {
+        // Then extract the parameters
+        expressions.forEach((expression) => {
+          // Build the definition with variable substitution
+          const substitution = buildSubstitution(expression, macro);
+          // Finally replace the declaration by the definition
+          result = result.replace(expression, substitution);
+        });
+      }
+    }
+  });
+  return result;
+}
+
+module.exports = function (content) {
+  content = content.toString('utf-8');
+  if (this.cacheable) this.cacheable();
+
+  var query = loaderUtils.parseQuery(this.query);
+  var config = {};
+
+  // Load config files
+  if (query.config !== undefined && query.config !== '') {
+    try {
+      config = JSON.parse(fs.readFileSync(query.config, 'utf8'));
+    } catch (e) {
+      console.error('exception:', e);
+      throw e;
+    }
+  }
+
+  // Replace user defined macros
+  if (config.macros !== undefined && config.macros.length > 0) {
+    config.macros = preprocessMacros(config.macros);
+    content = replaceMacros(content, config.macros);
+  }
+  // Replace __FILE__ by the file name
+  content = replaceFilename(content, this.resourcePath, config.reg);
+  // Replace __LINE__ by the line number
+  content = replaceLinenumber(content);
+  // TODO(JD): __FUNCTION__
+
+  return content;
+};
+module.exports.raw = true;
